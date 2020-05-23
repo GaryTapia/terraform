188389eb0b75a91a8b0b6923976569bc740d3240
GaryTapia <gary.tapia2009@gmail.com>
2020-05-22 20:55:23 -0500

    fill out provider, data and resource to create base Ubuntu image

---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+    region = "us-west-2"
+    shared_credentials_file = "C:\\Users\\Gurry\\.aws\\creds.txt"
+}
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+
+    filter {
+        name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"] # This is Cannonical
+}
+
+resource "aws_instance" "web" {
+    ami = "${data.aws_ami.ubuntu.id}"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "UbuntuServer"
+    }
+}
